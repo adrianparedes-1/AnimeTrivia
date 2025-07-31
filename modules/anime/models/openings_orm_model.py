9cddf0840ef30116fe6ad19ee4cd2686c78a4bbf
Adrian Paredes <adrianparedesroman99@gmail.com>
2025-07-31 18:00:15 -0400

    working on only adding unique values to the children tables
    learned a new way to query db to bulk query rather than query per item.
    Added it to a helper function but it is not complete yet. Will continue
    to work on it later.

--- modules/anime/models/openings_orm_model.py
+++ modules/anime/models/openings_orm_model.py
@@ -8,7 +8,7 @@ class Openings(Base):
     __tablename__ = "openings"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    title: Mapped[str] = mapped_column(String(300), server_default=None)
+    title: Mapped[str] = mapped_column(String(300), server_default=None, unique=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     deleted_at: Mapped[datetime] = mapped_column(server_default=None, nullable=True)
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.current_timestamp())
